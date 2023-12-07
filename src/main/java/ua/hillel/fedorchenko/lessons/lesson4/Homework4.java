c33648e19ccadccb76ab458b3f5278dba646c6ab
[Fedorchenko Vladyslav] <[vlad.fedorchenko@gmail.com]>
2023-12-07 15:13:42 +0100

    Homework4

---
+++ src/main/java/ua/hillel/fedorchenko/lessons/lesson4/Homework4.java
@@ -0,0 +1,18 @@
+package ua.hillel.fedorchenko.lessons.lesson4;
+
+public class Homework4 {
+    public static void main(String[] args) {
+         String name  = "Параллелепіпед";
+            System.out.println(name);
+            int a= 2;
+            int b= 3;
+            int c= 4;
+            int volume = a * b*c;
+            int length = a*4 + b*4 + c*4;
+            System.out.println("Сторона a = " + a );
+            System.out.println("Сторона b = " + b );
+            System.out.println("Сторона c = " + c );
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+            System.out.println("Cумарнa довжинa = " + length);
+    }
+}
